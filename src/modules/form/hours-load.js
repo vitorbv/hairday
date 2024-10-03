7c5e2b6e8bcfae6d97a9cfb4c10ebd13cf49da8c
Vitor Vianna <vitor.vianna.10@gmail.com>
2024-10-03 16:34:44 -0300

    Ajustando lista de horários disponíveis para dias futuros

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -6,6 +6,8 @@ import { hoursClick } from "./hours-click.js"
 const hours = document.getElementById("hours")
 
 export function hoursLoad( { date } ) {
+  // Limpa a lista de horarios.
+  hours.innerHTML = ""
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora
     const [scheduleHour] = hour.split(":")
