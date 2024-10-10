d47ec276841eba2205a013a3175b235d22f41a24
Vitor Vianna <vitor.vianna.10@gmail.com>
2024-10-09 21:07:57 -0300

    Integra com a API realizando o post ao realizar submit do form, e GET para exibir a lista de agendamentos

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,7 @@
 import dayjs from "dayjs"
 
+import { scheduleNew } from "../../services/schedule-new.js"
+
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
@@ -13,7 +15,7 @@ selectedDate.value = inputToday
 selectedDate.min = inputToday
 
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault()
 
   try {
@@ -40,7 +42,7 @@ form.onsubmit = (event) => {
     // Gera um ID
     const id = new Date().getTime()
 
-    console.log({
+    await scheduleNew({
       id,
       name,
       when,
--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,20 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
+import { schedulesShow } from "../schedules/show.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 // Seleciona o input de data
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // Obtém a data do input
   const date = selectedDate.value
+
+  // Busca na API os agendamentos.
+  const dailySchedules = await scheduleFetchByDay({ date })
+  
+  // exibe os agendamentos.
+  schedulesShow( { dailySchedules })
+
   // Renderiza as horas disponíveis
   hoursLoad( { date } )
 }
