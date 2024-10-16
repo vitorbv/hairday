54fae003ae381c38f90ca57c6429f4eebd7157ff
Vitor Vianna <vitor.vianna.10@gmail.com>
2024-10-16 18:34:31 -0300

    Atualiza a lista de agendamentos após o submit, bloqueia a hora quando já houver agendamento e limpa o campo do nome após o submit

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,22 +2,29 @@ import dayjs from "dayjs"
 
 import { openingHours } from "../../utils/opening-hours.js"
 import { hoursClick } from "./hours-click.js"
+import { schedulesDay } from "../schedules/load.js"
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad( { date } ) {
+export function hoursLoad( { date, dailySchedules } ) {
   // Limpa a lista de horarios.
   hours.innerHTML = ""
+
+  // Obtém a lista de todos os horários ocupados
+  const unavailableHours = dailySchedules.map((schedule) => dayjs(schedule.when).format("HH:mm"))
+
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora
     const [scheduleHour] = hour.split(":")
 
     // Adiciona a hora na date e verifica se já passou
-    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
+
+    const available = !unavailableHours.includes(hour) && !isHourPast
 
    return( {
       hour,
-      available: isHourPast
+      available,
     })
 
   })
--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 import { scheduleNew } from "../../services/schedule-new.js"
+import { schedulesDay } from "../schedules/load.js"
 
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
@@ -42,11 +43,18 @@ form.onsubmit = async (event) => {
     // Gera um ID
     const id = new Date().getTime()
 
+    // Faz o agendamento.
     await scheduleNew({
       id,
       name,
       when,
     })
+
+    // Recarrega a lista de agendamentos.
+    await schedulesDay()
+
+    // Limpa o campo de nome após o submit
+    clientName.value = ""
 
   } catch (error) {
     alert("Não foi possível realizar o agendamento.")
